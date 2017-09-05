b8633d8856ced8786dc942ccf2473e921339b991
DeanLee <lidian1@huawei.com>
2017-09-05 10:08:01 +0800

    new test file and info

--- componet/test/readfile.sh
+++ componet/test/readfile.sh
@@ -8,5 +8,8 @@ while read line
 	echo $fstr $sstr
   docker build -t containerops/$DN -f ./$line/Dockerfile ./$DN;
 	echo [docker build -t containerops/$line -f ./$line/Dockerfile ./$line/]
+	
+	sed "s/config:.*/config:abc/g" -i template
+	
 done < test2.md >>test3.md
 
